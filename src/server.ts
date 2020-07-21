41e79a4597890d1a0413a4b0ba222956519fdf71
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-21 13:13:04 -0300

    Design patterns with Eslint, Prettier and EditorConfig.

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,10 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello, World!'});
-})
-
 app.listen(3333, () => {
-    console.log('🚀 Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
