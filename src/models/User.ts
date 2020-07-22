9b51f30de7f36f3ceddbb01458ae2e2d8e1f8081
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-22 16:27:25 -0300

    CreateUser migration and User model created

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: number;
+
+  @CreateDateColumn()
+  // eslint-disable-next-line camelcase
+  created_at: Date;
+
+  @UpdateDateColumn()
+  // eslint-disable-next-line camelcase
+  updated_at: Date;
+}
+
+export default User;
