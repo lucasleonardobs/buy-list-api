74262778a04a9caf20fde9f6b625edaa126c7fbb
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-22 15:33:56 -0300

    CreateProduct migration created using TypeORM

---
+++ src/database/migrations/1595429086128-CreateProduct.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateProduct1595429086128 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'unitPrice',
+            type: 'decimal(6,2)',
+            isNullable: false,
+          },
+          {
+            name: 'category',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
