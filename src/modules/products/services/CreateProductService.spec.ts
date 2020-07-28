06a15b7a3b55e7a03a8ae58cf21e6c49ba295062
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-27 22:12:56 -0300

    Create product delete test

--- src/modules/products/services/CreateProductService.spec.ts
+++ src/modules/products/services/CreateProductService.spec.ts
@@ -1,5 +1,6 @@
 import FakeProductsRepository from '../repositories/fakes/FakeProductsRepository';
 import CreateProductService from './CreateProductService';
+import DeleteProductService from './DeleteProductService';
 
 describe('CreateProduct', () => {
   it('should be able to create a new product', async () => {
@@ -18,5 +19,26 @@ describe('CreateProduct', () => {
     expect(product.description).toBe('teste');
     expect(product.unitPrice).toBe(123);
     expect(product.category).toBe('teste');
+  });
+});
+
+describe('DeleteProduct', () => {
+  it('should be able to delete a new product', async () => {
+    const fakeProductsRepository = new FakeProductsRepository();
+    const createProduct = new CreateProductService(fakeProductsRepository);
+    const deleteProduct = new DeleteProductService(fakeProductsRepository);
+
+    const productDeleteTest = await createProduct.execute({
+      name: 'teste',
+      description: 'teste',
+      unitPrice: 123,
+      category: 'teste',
+    });
+
+    const { id } = productDeleteTest;
+    await deleteProduct.execute({ id });
+    const find = await fakeProductsRepository.findOne({ id });
+
+    expect(find).toBeUndefined();
   });
 });
