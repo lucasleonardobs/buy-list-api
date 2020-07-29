9d554165181a9b54dff64c0647315af0a99136a1
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-29 19:22:36 -0300

    Add new methods to finals end-points.

--- src/modules/products/repositories/IOrdersRepository.ts
+++ src/modules/products/repositories/IOrdersRepository.ts
@@ -1,8 +1,17 @@
 import Order from '../infra/typeorm/entities/Order';
+
 import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
+import IUpdateOrderDTO from '../dtos/IUpdateOrderDTO';
+import IDeleteOrderDTO from '../dtos/IDeleteOrderDTO';
+
+import ICheckOrdersExists from '../dtos/ICheckOrdersExists';
 
 interface IOrdersRepository {
   create(data: ICreateOrderDTO): Promise<Order>;
+  update(data: IUpdateOrderDTO): Promise<Order>;
+  delete(data: IDeleteOrderDTO): Promise<void>;
+  findOne(id: ICheckOrdersExists): Promise<Order | undefined>;
+  find(): Promise<Order[]>;
 }
 
 export default IOrdersRepository;
--- src/modules/products/repositories/IProductsRepository.ts
+++ src/modules/products/repositories/IProductsRepository.ts
@@ -1,11 +1,13 @@
 import ICreateProductDTO from '../dtos/ICreateProductDTO';
 import IDeleteProductDTO from '../dtos/IDeleteProductDTO';
 import ICheckProductsExists from '../dtos/ICheckProductsExists';
+import IUpdateProductDTO from '../dtos/IUpdateProductDTO';
 
 import Product from '../infra/typeorm/entities/Product';
 
 interface IProductsRepository {
   create(data: ICreateProductDTO): Promise<Product>;
+  update(data: IUpdateProductDTO): Promise<Product>;
   delete(id: IDeleteProductDTO): Promise<void>;
   findOne(id: ICheckProductsExists): Promise<Product | undefined>;
   find(): Promise<Product[]>;
