3f4004c02f3602269bc399d13da1987fbd44d9c2
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-29 19:20:39 -0300

    Create service to show one product.

---
+++ src/modules/products/services/ShowOneProductService.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+import IProductsRepository from '../repositories/IProductsRepository';
+import Product from '../infra/typeorm/entities/Product';
+
+interface IRequest {
+  id: number;
+}
+
+@injectable()
+class ListProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({ id }: IRequest): Promise<Product> {
+    const product = await this.productsRepository.findOne({ id });
+
+    if (!product) {
+      throw new AppError('Product not found', 404);
+    }
+
+    return product;
+  }
+}
+
+export default ListProductService;
