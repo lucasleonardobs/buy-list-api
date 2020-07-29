cfff06c937fe6362c28751e10978d21f415b7e84
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-29 19:30:56 -0300

    Finalize apply dependency injection.

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from 'express';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import BCryptHashProvider from '@modules/users/providers/HashProvider/implementations/BCryptHashProvider';
 import UsersRepository from '../../typeorm/repositories/UsersRepository';
 
 class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const usersRepository = new UsersRepository();
+    const hashProvider = new BCryptHashProvider();
     const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService(usersRepository);
+    const authenticateUser = new AuthenticateUserService(
+      usersRepository,
+      hashProvider,
+    );
 
     const { user, token } = await authenticateUser.execute({
       email,
