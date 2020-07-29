c9d2af0593edd47c3d061e2b2669c20601f9fdb3
Lucas Leonardo <llbs@cin.ufpe.br>
2020-07-29 19:27:57 -0300

    Finish controllers methods.

--- src/modules/products/infra/http/controllers/OrdersController.ts
+++ src/modules/products/infra/http/controllers/OrdersController.ts
@@ -3,6 +3,9 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateOrderService from '@modules/products/services/CreateOrderService';
+import DeleteOrderService from '@modules/products/services/DeleteOrderService';
+import UpdateOrderService from '@modules/products/services/UpdateOrderService';
+import ListOrdersService from '@modules/products/services/ListOrdersService';
 
 class OrdersController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -18,6 +21,38 @@ class OrdersController {
     });
 
     return response.json(order);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { quantity, total_cost, product_id } = request.body;
+
+    const updateOrder = container.resolve(UpdateOrderService);
+
+    const update = await updateOrder.execute({
+      id,
+      quantity,
+      total_cost,
+      product_id,
+    });
+
+    return response.json(update);
+  }
+
+  public async delete(request: Request, _: Response): Promise<void> {
+    const { id } = request.params;
+
+    const deleteOrder = container.resolve(DeleteOrderService);
+
+    deleteOrder.execute({ id });
+  }
+
+  public async show(_: Request, response: Response): Promise<Response> {
+    const listOrderService = container.resolve(ListOrdersService);
+
+    const orders = await listOrderService.execute();
+
+    return response.json(orders);
   }
 }
 
--- src/modules/products/infra/http/controllers/ProductsController.ts
+++ src/modules/products/infra/http/controllers/ProductsController.ts
@@ -3,9 +3,20 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateProductService from '@modules/products/services/CreateProductService';
+import UpdateProductService from '@modules/products/services/UpdateProductService';
 import DeleteProductService from '@modules/products/services/DeleteProductService';
+import ListProductService from '@modules/products/services/ListProductService';
+import ShowOneProductService from '@modules/products/services/ShowOneProductService';
 
 class ProductsController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const listProductService = container.resolve(ListProductService);
+
+    const products = await listProductService.execute();
+
+    return response.json(products);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, description, unitPrice, category } = request.body;
 
@@ -29,6 +40,33 @@ class ProductsController {
     await deleteProduct.execute({ id: Number(id) });
 
     return response.json({ message: 'Delete has been successful.' });
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showOneProduct = container.resolve(ShowOneProductService);
+
+    const product = await showOneProduct.execute({ id: Number(id) });
+
+    return response.json(product);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, description, unitPrice, category } = request.body;
+
+    const updateProduct = container.resolve(UpdateProductService);
+
+    const update = await updateProduct.execute({
+      id: Number(id),
+      name,
+      description,
+      unitPrice,
+      category,
+    });
+
+    return response.json(update);
   }
 }
 
